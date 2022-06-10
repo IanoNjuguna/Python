3a883e8dd572580af744db9766ff01d3cfc175e6
IanoNjuguna <ichege438@gmail.com>
2022-06-10 17:18:34 +0300

    Comparison-Operator(s)-Code

---
+++ 1-StackUp-Notes/Basics-1/Comparison-Operators.py
@@ -0,0 +1,48 @@
+# Comparison Operators
+
+# {i} equal to operator
+# (a) true
+print(1==1)
+
+# (b) false
+print(1==2)
+
+# (c) comparing strings;{true}
+print("hello"=="hello")
+
+# (d) comparing strings;{false}
+print("hello"=="helo")
+
+
+# {ii} not equal to operator
+#(a) true
+print(1!=2)
+
+#(b) false
+print(2!=2)
+
+#(c) comparing strings;{true}
+print("hello"!="helo")
+
+
+# {iii} more than operator
+# (a) true
+print(2>1)
+
+# (b) false
+print(1>2)
+
+
+# {iv} more than OR equal to operator
+# (a) true
+print(2>=2)
+
+
+# {v} less than operator
+# (a) true
+print(1<2)
+
+
+# {vi} less than OR equal to operator
+# (a) true
+print(1<=1)
