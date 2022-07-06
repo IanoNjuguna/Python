f60eb2f016ad719d295ebf8ae476606f6b10329e
IanoNjuguna <ichege438@gmail.com>
2022-07-06 16:43:24 +0300

    Added code on getting a list

---
+++ 1-StackUp-Notes/Basics-2/Getting-a-List-Item.py
@@ -0,0 +1,9 @@
+letters = ["a", "b", "c", "d", "e"]
+# To get the an item in a list, consider:
+# "a", "b", "c", "d", "e"
+#  0    1    2    3    4
+# -5   -4   -3   -2   -1
+
+print(letters[0])
+print(letters[-3])
+print(letters[-1])
