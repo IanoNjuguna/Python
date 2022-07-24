54a750d07003a082d4e0ea5842780fbb1bede840
IanoNjuguna <ichege438@gmail.com>
2022-07-24 15:44:44 +0300

    Loop-Control-1 dot py

---
+++ 1-StackUp-Notes/Basics-2/Loop-Control-1.py
@@ -0,0 +1,17 @@
+# To print items in a list one after the other:
+letters= ["a", "b", "c", "d", "e"]
+
+for letter in letters:
+    print(letter)
+
+# To print a specific part of the list(slice):
+for letter in letters [1:4]:
+    print(letter)
+    
+# To get use a certain block of code repeatedly:
+# range()
+for number in range (9):
+    print("Things will get better")
+
+for number in range (5):
+    print(number)
