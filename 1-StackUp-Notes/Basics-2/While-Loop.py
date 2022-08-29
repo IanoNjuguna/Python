b5ff87c4ab0c4ee48cf37434efb46a68cd12205f
IanoNjuguna <ichege438@gmail.com>
2022-08-29 17:39:13 +0300

    WhileLoop.py

---
+++ 1-StackUp-Notes/Basics-2/While-Loop.py
@@ -0,0 +1,9 @@
+# While a particular condition evaluates to true,
+# the code block beneath will continuously be executed. So:
+# while True:
+#       code will continuously run
+
+test=-2
+while test<10:
+    print("test is <10")
+    test+=1 # adds 1 to test
