5b32b9849f47fa6ef882c217442524c3622c4100
IanoNjuguna <inc3099@gmail.com>
2023-01-09 20:01:42 +0300

    Py4e: Variables, constants, expressions, arithmetic

---
+++ 3-PY4E/variables-expressions_statements/ex02.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+
+# Prompt user for name and welcome them
+
+name = input("Enter your name: ")
+print("Hello", name)
---
+++ 3-PY4E/variables-expressions_statements/ex03.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+hrs = input("Enter Hours:")
+
+rate = input("Rate per hour in $: ")
+
+grosspay = float(hrs) * float(rate)
+
+print("Pay:", grosspay)
