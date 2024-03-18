8fd0c3597903c831152af06e25caa8666634b260
IanoNjuguna <ichege438@gmail.com>
2024-03-18 14:39:52 +0300

    intro to Lua

---
+++ 02-lua/00-basics/01-variables.lua
@@ -0,0 +1,16 @@
+-- assigning variables
+
+score = 100
+print(score)
+
+-- Variables don't hold a type. The value in the variable has a type (Lua is dynamically typed).
+
+-- 7 Lua Data types: nil (similar to undefined in JS), strings, numbers, functions, CFunctions, userdata, table.
+
+-- type() exists
+
+-- string formatting
+print("The value of score is "..score)
+
+name = "Iano"
+print("The value of name is "..name)
